f3ddcb8656b787ff94b7d113ecc7a350a013c400
Chris <mail@christopherparsons.me>
2017-09-13 21:21:26 +0100

    Updated check function to work on every item of an array with a for loop. Refactored the button pushes to one function that check a data-value key on the html side.

--- simon.js
+++ simon.js
@@ -4,6 +4,7 @@ $(document).ready(
 
     var initial = Math.floor((Math.random()*4) + 1);
     var seq = [initial];
+    // Upon page load, a new sequence is created. Will likely adjust this to be on press of a start button in future.
     console.log(seq);
     // Declared outside of the function to prevent it being over-written on each button press.
     var comp = [];
@@ -12,100 +13,38 @@ $(document).ready(
     function check () {
       var correct = true;
       for (var i=0; i <= comp.length-1; i++) {
-        if (seq[i] == comp[i]) {
-
-          // Generates a random number between 1 and 4
-          console.log('Correct');
-        } else {
-          console.log(seq);
-          console.log(comp);
+        // For loop to check each item in the sequence
+        if (seq[i] !== comp[i]) {
           correct = false;
         }
+        // To save adding an if and an else, have used !== and initialised correct as false.
       };
-      console.log(correct)
       var selected = Math.floor((Math.random()*4) + 1);
       if (seq.length == comp.length && correct == true) {
         seq.push(selected);
-        console.log(seq)
         comp = [];
+        // Resetting the comparison array upon a successful sequence.
+        alert('Correct');
       } else if (correct == false) {
-        alert('Incorrect');
+        var score = seq.length - 1;
+        alert('Game Over. Final score: ' + score + '. Click OK to try again.');
         seq = [selected];
+        // Starts a new game with a fresh sequence.
         comp = [];
+        // Resets the comparison array to be blank for a new game.
       }
 
 
     };
 
-    //   } else
-    //   if (seq.join('') == comp.join('')) {
-    //     var selected = Math.floor((Math.random()*4) + 1);
-    //     // Generates a random number between 1 and 4
-    //     seq.push(selected);
-    //     console.log(seq);
-    //     // Pushes the randomly generated number into the sequence
-    //     alert('Correct! Can you remember this many?')
-    //     comp = [];
-    //     console.log(comp);
-    //     // This resets the comparison array. Currently stops the game progressing.
-    //   } else {
-    //     alert('Incorrect. Game Over')
-    //     // If the two arrays do not match, then a game over message is presented.
-    //   }
-    //   console.log(seq);
-    //   console.log(comp);
-    // };
-
-    $('#red1').click(
-      function red1 () {
-        comp.push(1);
-        // Adds a 1 to the empty comparison array.
-        if (seq.length == comp.length) {
-          check ();
-        } else {
-        }
-        // Checks to see if the sequence and the comparison array are the same length.
-        // If they are, compares the two arrays.
-        // If not, allows user to continue entry.
-      }
-    )
-
-    $('#blue2').click(
-      function blue2 () {
-        comp.push(2);
-        // Adds a 2 to the empty comparison array.
-        if (seq.length == comp.length) {
-          check ();
-        }
-        // Checks to see if the sequence and the comparison array are the same length.
-        // If they are, compares the two arrays.
-        // If not, allows user to continue entry.
-      }
-    )
-
-    $('#green3').click(
-      function green3 () {
-        comp.push(3);
-        // Adds a 3 to the empty comparison array.
-        if (seq.length == comp.length) {
-          check ();
-        }
-        // Checks to see if the sequence and the comparison array are the same length.
-        // If they are, compares the two arrays.
-        // If not, allows user to continue entry.
-      }
-    )
-
-    $('#yellow4').click(
-      function yellow4 () {
-        comp.push(4);
-        // Adds a 4 to the empty comparison array.
-        if (seq.length == comp.length) {
-          check ();
-        }
-        // Checks to see if the sequence and the comparison array are the same length.
-        // If they are, compares the two arrays.
-        // If not, allows user to continue entry.
+    $('button').click(
+      // targetting the button id to avoid having to repeat code.
+      function buttonPress () {
+        console.log(this.dataset.value);
+        comp.push(parseInt(this.dataset.value));
+        // buttons all have a data-value with their id numbers 1-4. This pulls the matching number from the button.
+        check ();
+        // Runs the check function to confirm if the button press is correct.
       }
     )
 
